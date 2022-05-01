3565453efbe479f17d50b648d1c495758be4f7ef
mentalist <sbbhopaleshubham55@gmail.com>
2022-05-01 23:26:24 +0530

    Added the JUnit 5 dependency and created a Test class.

---
+++ src/test/java/tddassessment/TestStringCalculator.java
@@ -0,0 +1,14 @@
+package tddassessment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestStringCalculator {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
