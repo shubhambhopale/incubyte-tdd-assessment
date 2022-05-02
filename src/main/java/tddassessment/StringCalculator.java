72ec73a872b899f20cabec90bab8ebab133dac7d
mentalist <sbbhopaleshubham55@gmail.com>
2022-05-02 23:59:04 +0530

    Made 6th failing test-case run successfully for a string containing Negative Number(s) scenario.

--- src/main/java/tddassessment/StringCalculator.java
+++ src/main/java/tddassessment/StringCalculator.java
@@ -1,38 +1,54 @@
 package tddassessment;
 
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
 	public static void main(String[] args) {
 		// TODO: Need to write a logic here.
-		
-		//StringCalculator calculator = new StringCalculator();		
-		//System.out.println(calculator.add("14;2\n7,//;"));
+
+		// StringCalculator calculator = new StringCalculator();
+		// try {
+		// System.out.println(calculator.add("2,3,-4\n5,-2"));
+		// } catch (NegativeNumberException e) {
+		// e.printStackTrace();
+		// }
 	}
 
-	public Integer add(String numbers) {
+	public Integer add(String numbers) throws NegativeNumberException {
 		int sum = 0;
-		
-		StringBuilder delimeterString = new StringBuilder(",\\n");
-		
-		if(numbers.contains("//")) {
-			delimeterString.append(numbers.charAt(numbers.indexOf("//")+2));
-		}
-		
-		if(!(numbers.isBlank() && numbers.isEmpty())) {
-			String[] numArray = numbers.split("["+ delimeterString +"]+");
-			
-			for(String value : numArray) {
-				if(value.matches("[0-9]+"))
+
+		if (!(numbers.isBlank() && numbers.isEmpty())) {
+			StringBuilder delimeterString = new StringBuilder(",\\n");
+
+			if (numbers.contains("//")) {
+				delimeterString.append(numbers.charAt(numbers.indexOf("//") + 2));
+				numbers = numbers.replace("//" + numbers.charAt(numbers.indexOf("//") + 2), "");
+			}
+
+			String[] numArray = numbers.split("[" + delimeterString + "]+");
+
+			Predicate<String> isNegative = num -> Integer.parseInt(num) < 0;
+			if (Stream.of(numArray).anyMatch(isNegative)) {
+				throw new NegativeNumberException("Negatives not allowed - "
+						+ Stream.of(numArray).filter(isNegative).collect(Collectors.toList()));
+			}
+
+			for (String value : numArray) {
+				if (value.matches("[0-9]+"))
 					sum += Integer.parseInt(value);
 			}
 		}
-		
+
 		return sum;
 	}
 
 }
 
-class NegativeNumberException extends Exception{
+@SuppressWarnings("serial")
+class NegativeNumberException extends Exception {
 
 	public NegativeNumberException(String message) {
 		super(message);
--- src/test/java/tddassessment/TestStringCalculator.java
+++ src/test/java/tddassessment/TestStringCalculator.java
@@ -16,28 +16,28 @@ class TestStringCalculator {
 	}
 
 	@Test
-	public void testEmptyStringWithCalculator() {
+	public void testEmptyStringWithCalculator() throws NegativeNumberException {
 		assertEquals(0, calculator.add(""), "Should return a 0 for EMPTY string.");
 	}
 
 	@Test
-	public void testSingleNumberStringWithCalculator() {
+	public void testSingleNumberStringWithCalculator() throws NegativeNumberException {
 		assertEquals(5, calculator.add("5"), "Should return proper addition for the number(s).");
 	}
 
 	@Test
-	public void testSeveralNumberStringWithCalculator() {
+	public void testSeveralNumberStringWithCalculator() throws NegativeNumberException {
 		assertEquals(23, calculator.add("14,2,7"), "Should return proper addition for the number(s).");
 	}
 
 	@Test
-	public void testNewLineBetweenNumbersStringWithCalculator() {
+	public void testNewLineBetweenNumbersStringWithCalculator() throws NegativeNumberException {
 		assertEquals(36, calculator.add("6\n21,9"),
 				"Should return proper addition for the number(s) even with a New Line character in between.");
 	}
 
 	@Test
-	public void testNewDelimiterStringWithCalculator() {
+	public void testNewDelimiterStringWithCalculator() throws NegativeNumberException {
 		assertEquals(36, calculator.add("//;6\n21,9"),
 				"Should return proper addition for the number(s) provided a new delimiter in a string.");
 	}
