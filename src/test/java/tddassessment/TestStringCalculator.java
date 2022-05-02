4b6799f31739e8224044397f3850cfbf22a81fdb
mentalist <sbbhopaleshubham55@gmail.com>
2022-05-02 20:08:04 +0530

    Created 2nd failing test-case for Single number string scenario.

--- src/test/java/tddassessment/TestStringCalculator.java
+++ src/test/java/tddassessment/TestStringCalculator.java
@@ -6,16 +6,21 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class TestStringCalculator {
-	
+
 	static StringCalculator calculator;
-	
+
 	@BeforeAll
 	static void setUp() {
 		calculator = new StringCalculator();
 	}
-	
+
 	@Test
 	public void testEmptyStringWithCalculator() {
 		assertEquals(0, calculator.add(""), "Should return a 0 for EMPTY string.");
+	}
+
+	@Test
+	public void testSingleNumberStringWithCalculator() {
+		assertEquals(5, calculator.add("5"), "Should return proper addition for the number(s).");
 	}
 }
